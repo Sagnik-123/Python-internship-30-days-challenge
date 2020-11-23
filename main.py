c356cc0c72f3fa978713a4f25df034115222c150
Sagnik-123 <74890765+Sagnik-123@users.noreply.github.com>
2020-11-23 17:23:02 +0530

    First Day Task

---
+++ main.py
@@ -0,0 +1,51 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print('Hello World') #Printing Hello,World
+
+print("This is my First Python program")
+
+print('30 days 30 hour challenge')
+
+Hours="thirty" # inialising sting to a normal variable
+print(Hours)
+
+#Indexing using String:
+Days="Thirty days"
+print(Days[1])
+
+#Print Particular character from certain text
+
+challenge="My name is Sagnik Bhattacharya"
+print(challenge[11:])
+
+#Print the length of Character:
+challenge="My name is Sagnik Bhattacharya"
+print(len(challenge))
+
+#Convert String into Lower case:
+challenge="My FIRST ProGRam"
+print(challenge.lower())
+
+#String Concatenation:
+a="WE LIVE"
+b=" IN INDIA"
+c=a+b
+print(c)
+
+#Adding Spaces in concatenation:
+
+d="I LOVE"
+e="MY COUNTRY"
+print(d+" "+e)
+
+#Casefold usage:
+text="Thirty Days Thirty Hours Internship"
+x=text.casefold()
+print(x)
+
+
